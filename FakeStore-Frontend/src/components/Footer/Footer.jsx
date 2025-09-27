dfe72ed677e4cb164ea09ffeef3edd30beca91e6
caio silva campos corgozinho <104537051+csccorgozinho@users.noreply.github.com>
2025-09-27 14:42:16 -0300

    Add files via upload

---
+++ FakeStore-Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+// src/components/Footer/Footer.jsx
+import {useContext} from 'react'
+import "./Footer.css"
+import { ThemeContext } from '../../contexts/DarkModeContext';
+import { Link } from 'react-router-dom';
+import { FaGithubSquare, FaLinkedin} from "react-icons/fa";
+
+function Footer() {
+   const {darkMode} = useContext(ThemeContext);
+  return (
+    <div className={
+      darkMode?
+      'footer-container'
+      :
+      'footer-container dark-footer-container'
+    }>
+      <div className='social-icon-container'>
+        <Link to="https://github.com/1uckyswish?tab=repositories"> <FaGithubSquare className='social-icons'/></Link>
+        <Link to="https://www.linkedin.com/in/noel-guillen-blas-b63353257/?trk=people-guest_people_search-card"><FaLinkedin className='social-icons'/></Link>
+      </div>
+      <Link to='/contactUs'>Contact Us</Link>
+      <Link to='/myorders'>Meus Pedidos</Link> {/* Novo link */}
+    </div>
+  )
+}
+
+export default Footer
