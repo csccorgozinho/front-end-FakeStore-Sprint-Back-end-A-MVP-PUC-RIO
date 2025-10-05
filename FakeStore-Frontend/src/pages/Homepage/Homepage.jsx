286ac0c6cc9dd0cfc3f2a85eceb96fc46fade29d
caio silva campos corgozinho <104537051+csccorgozinho@users.noreply.github.com>
2025-10-05 10:30:53 -0300

    Update Homepage.jsx

--- FakeStore-Frontend/src/pages/Homepage/Homepage.jsx
+++ FakeStore-Frontend/src/pages/Homepage/Homepage.jsx
@@ -10,23 +10,21 @@ function Homepage() {
   const [categories, setCategories] = useState([]);
   const [product, setProducts] = useState([]);
 
-  //** toggle dark mode */
-  //** use context for global state */
+
   const {darkMode} = useContext(ThemeContext);
 
-  //* Load the categories and products when page loads
+
   useEffect(
     ()=>{
-      //*get the data from the api
+      
    axios.get(`https://fakestoreapi.com/products/categories`)
   .then((res)=>{
-    //* These are the categories from api. What do i do with them next?
-    //* store data in state
+    
     setCategories(res.data);
   })
   .catch((error) => console.log(error))
 
-  //* make the second api call for products
+ 
   axios.get(`https://fakestoreapi.com/products`)
   .then((result) => {
     setProducts(result.data);
@@ -35,15 +33,14 @@ function Homepage() {
     }, []);
 
 
-  //* handle a filter button system to load different categories for products
-//* https://fakestoreapi.com/products/category/women's%20clothing
+
   const handleFilter = (category) => {
     let url = "https://fakestoreapi.com/products"
     category != "All" ? url =`https://fakestoreapi.com/products/category/${category}` :  url
-    //* grab the value of every button to create a query
+    
     axios.get(url)
     .then(productResult => {
-      //* get the data from the api and append it to the product card
+      
       setProducts(productResult.data)
     })
     .catch((productError) => console.log(productError));
@@ -62,15 +59,12 @@ function Homepage() {
         'category-container dark-category-container'
       }>
         <p className='category' type='all' onClick={()=> handleFilter("All")}>All</p>
-        {  //* using the map function to make a <p> tag for each category
-        //* grabbing the index of the first category we are able to get the first letter
-        //* get the string value and append the value of the capital letter and add it to the position of the word by using slice to remove the first lower case letter.
+        {  
         categories.map((element) => <p onClick={()=> handleFilter(element)} key={element}>{element.charAt(0).toUpperCase() + element.slice(1)}</p>)
         }
       </div>
       <div className='product-container'>
-        { //* call the component "ProductCard" to append the html to container
-          //* apply a prop to the component to receive the data as well as apply an ID
+        { 
          product.map((item) => <ProductCard products={item} key={item.id} />)
         }
       </div>
